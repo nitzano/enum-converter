c383c3685c658bc9b22692afbe47ef0505084d4f
nitzan <this.vptr@gmail.com>
2018-05-06 15:46:49 +0300

    added json dumper cli test

---
+++ __tests__/samples/cli/python.cli.sample.py
@@ -0,0 +1,14 @@
+''' Test file for cli tester '''
+from enum import Enum, auto, IntEnum
+
+
+class Colors(Enum):
+    Green = 1
+    Yellow = 2
+    Blue = 5
+
+
+class Pets(Enum):
+    Dog = 1
+    Cat = 2
+    Mouse = 4
--- src/cli/cli.spec.ts
+++ src/cli/cli.spec.ts
@@ -1,8 +1,11 @@
 import { execSync } from 'child_process';
+import { resolve } from 'path';
+import { readFileSync } from 'fs';
 const packageJson = require('../../package.json');
 
 describe('CLI', () => {
   let cliPath: string;
+  const samplesPath = resolve(`${__dirname}/../../__tests__/samples`);
 
   beforeAll(() => {
     const binPath = execSync('npm bin')
@@ -19,5 +22,19 @@ describe('CLI', () => {
       .trim();
     const packageVersion = packageJson.version;
     expect(cliVersion).toBe(packageVersion);
+  });
+
+  it('should convert python to a valid json', () => {
+    const cliPythonSamplePath: string = `${samplesPath}/cli/python.cli.sample.py`;
+    const jsonEnum: string = execSync(
+      `${cliPath} ${cliPythonSamplePath} --to json`
+    ).toString();
+    const jsonEnumString = JSON.parse(jsonEnum);
+
+    const jsonSample = JSON.parse(
+      String(readFileSync(`${samplesPath}/cli/json.cli.sample.json`))
+    );
+
+    expect(jsonSample).toEqual(jsonEnumString);
   });
 });
