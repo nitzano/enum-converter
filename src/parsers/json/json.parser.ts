8ef9927d3ce98cd4d54ff1928489120e7a40445a
nitzan <this.vptr@gmail.com>
2018-05-07 13:01:45 +0300

    coded json parser

--- src/parsers/json/json.parser.ts
+++ src/parsers/json/json.parser.ts
@@ -24,11 +24,29 @@ export class JsonParser extends FileParser {
   tmpFileSuffix: string = 'json';
 
   extractEntries(filePath: string): EnumEntry[] {
+    const enumEntries: EnumEntry[] = [];
     if (filePath) {
       const jsonData: object = JSON.parse(this.getData(filePath));
-      console.log('json data', jsonData);
+
+      Object.entries(jsonData).forEach(([enumName, values]) => {
+        const enumValues: EnumValue[] = Object.entries(values).map(
+          ([enumKey, enumValue]) => {
+            if (
+              ['number', 'boolean', 'string'].includes(typeof enumValue) ||
+              enumValue === null
+            ) {
+              return new EnumValue(enumKey, enumValue as EnumValueType);
+            } else {
+              throw new Error(`could not cast enum value ${enumValue}`);
+            }
+          }
+        );
+        const enumEntry = new EnumEntry(enumName, enumValues);
+
+        enumEntries.push(enumEntry);
+      });
     }
 
-    return [];
+    return enumEntries;
   }
 }
