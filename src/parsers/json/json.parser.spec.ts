0f14577bd88b1c6b4d746ee4413f9313d82735ff
nitzan <this.vptr@gmail.com>
2018-05-07 13:33:17 +0300

    added json parser tests

--- src/parsers/json/json.parser.spec.ts
+++ src/parsers/json/json.parser.spec.ts
@@ -2,16 +2,22 @@ import * as path from 'path';
 
 import { EnumValue } from '../../models/enum-value/enum-value.model';
 import { JsonParser } from './json.parser';
+import { EnumFile } from '../../models/enum-file/enum-file.model';
 
 describe('Json Parser', () => {
-  it('should parse the basic sample', () => {
+  let parser: JsonParser;
+
+  beforeEach(() => {
+    parser = new JsonParser();
+  });
+
+  it('should parse the basic sample file', () => {
     const samplePath = path.resolve(
       __dirname,
       '../../../__tests__/samples/basic/json.basic.sample.json'
     );
-    const parser = new JsonParser();
     parser.parse(samplePath);
-    const enumFile = parser.enumFile;
+    const enumFile: EnumFile = parser.enumFile;
 
     expect(enumFile.entries).toHaveLength(2);
 
@@ -22,27 +28,163 @@ describe('Json Parser', () => {
     expect(enumFile.entries[1].values).toHaveLength(3);
   });
 
-  xit('should parse from string', () => {
-    expect(true).toBe(false);
+  it('should parse from string', () => {
+    const sampleObj: object = {
+      Colors: {
+        Blue: 'Blue',
+        Green: 1,
+        Yellow: 'Yellow'
+      }
+    };
+
+    parser.parseString(JSON.stringify(sampleObj));
+    const enumFile: EnumFile = parser.enumFile;
+
+    expect(enumFile.entries).toHaveLength(1);
+
+    expect(enumFile.entries[0].name).toBe('Colors');
+    expect(enumFile.entries[0].values).toHaveLength(3);
   });
 
-  xit('should parse numeric values', () => {
-    expect(true).toBe(false);
+  it('should parse numeric values', () => {
+    const sampleObj: object = {
+      Colors: {
+        Blue: 1,
+        Green: 2,
+        Yellow: 5
+      }
+    };
+
+    parser.parseString(JSON.stringify(sampleObj));
+    const enumFile: EnumFile = parser.enumFile;
+
+    expect(enumFile.entries).toHaveLength(1);
+
+    expect(enumFile.entries[0].name).toBe('Colors');
+    expect(enumFile.entries[0].values).toHaveLength(3);
+
+    const values: EnumValue[] = enumFile.entries[0].values;
+
+    expect(values[0].name).toBe('Blue');
+    expect(values[0].value).toBe(1);
+    expect(values[1].name).toBe('Green');
+    expect(values[1].value).toBe(2);
+    expect(values[2].name).toBe('Yellow');
+    expect(values[2].value).toBe(5);
   });
 
-  xit('should parse string values', () => {
-    expect(true).toBe(false);
+  it('should parse string values', () => {
+    const sampleObj: object = {
+      Colors: {
+        Blue: 'Blue',
+        Green: 'Green',
+        Yellow: 'Yellow'
+      }
+    };
+
+    parser.parseString(JSON.stringify(sampleObj));
+    const enumFile: EnumFile = parser.enumFile;
+
+    expect(enumFile.entries).toHaveLength(1);
+
+    expect(enumFile.entries[0].name).toBe('Colors');
+    expect(enumFile.entries[0].values).toHaveLength(3);
+
+    const values: EnumValue[] = enumFile.entries[0].values;
+
+    expect(values[0].name).toBe('Blue');
+    expect(values[0].value).toBe('Blue');
+    expect(values[1].name).toBe('Green');
+    expect(values[1].value).toBe('Green');
+    expect(values[2].name).toBe('Yellow');
+    expect(values[2].value).toBe('Yellow');
   });
 
-  xit('should parse boolean values', () => {
-    expect(true).toBe(false);
+  it('should parse boolean values', () => {
+    const sampleObj: object = {
+      Colors: {
+        Blue: true,
+        Green: false,
+        Yellow: true
+      }
+    };
+
+    parser.parseString(JSON.stringify(sampleObj));
+    const enumFile: EnumFile = parser.enumFile;
+
+    expect(enumFile.entries).toHaveLength(1);
+
+    expect(enumFile.entries[0].name).toBe('Colors');
+    expect(enumFile.entries[0].values).toHaveLength(3);
+
+    const values: EnumValue[] = enumFile.entries[0].values;
+
+    expect(values[0].name).toBe('Blue');
+    expect(values[0].value).toBe(true);
+    expect(values[1].name).toBe('Green');
+    expect(values[1].value).toBe(false);
+    expect(values[2].name).toBe('Yellow');
+    expect(values[2].value).toBe(true);
   });
 
-  xit('should parse automatic values', () => {
-    expect(true).toBe(false);
+  it('should parse automatic values', () => {
+    const sampleObj: object = {
+      Colors: {
+        Blue: 1,
+        Green: null,
+        Yellow: 5
+      }
+    };
+
+    parser.parseString(JSON.stringify(sampleObj));
+    const enumFile: EnumFile = parser.enumFile;
+
+    expect(enumFile.entries).toHaveLength(1);
+
+    expect(enumFile.entries[0].name).toBe('Colors');
+    expect(enumFile.entries[0].values).toHaveLength(3);
+
+    const values: EnumValue[] = enumFile.entries[0].values;
+
+    expect(values[0].name).toBe('Blue');
+    expect(values[0].value).toBe(1);
+    expect(values[0].isAutomatic).toBe(false);
+
+    expect(values[1].name).toBe('Green');
+    expect(values[1].isAutomatic).toBe(true);
+
+    expect(values[2].name).toBe('Yellow');
+    expect(values[2].value).toBe(5);
+    expect(values[2].isAutomatic).toBe(false);
   });
 
-  xit('should parse mixed values', () => {
-    expect(true).toBe(false);
+  it('should parse mixed values', () => {
+    const sampleObj: object = {
+      Colors: {
+        Blue: 'Blue',
+        Green: 1,
+        Red: null,
+        Yellow: false
+      }
+    };
+
+    parser.parseString(JSON.stringify(sampleObj));
+    const enumFile: EnumFile = parser.enumFile;
+
+    expect(enumFile.entries).toHaveLength(1);
+
+    expect(enumFile.entries[0].name).toBe('Colors');
+    expect(enumFile.entries[0].values).toHaveLength(4);
+
+    const values: EnumValue[] = enumFile.entries[0].values;
+
+    expect(values[0].name).toBe('Blue');
+    expect(values[0].value).toBe('Blue');
+    expect(values[1].name).toBe('Green');
+    expect(values[1].value).toBe(1);
+    expect(values[2].name).toBe('Red');
+    expect(values[2].isAutomatic).toBe(true);
+    expect(values[3].name).toBe('Yellow');
+    expect(values[3].value).toBe(false);
   });
 });
