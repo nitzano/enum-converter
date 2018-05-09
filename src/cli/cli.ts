75498a7c9d683528c3b3c5683e436b37d1dbe96d
nitzan <this.vptr@gmail.com>
2018-05-09 23:43:23 +0300

    merged parse-args  to cli.ts

--- src/cli/cli.ts
+++ src/cli/cli.ts
@@ -1,6 +1,31 @@
 #!/usr/bin/env node
-
+import { writeFileSync } from 'fs';
+import { Arguments } from 'yargs';
+import { ConfigurationOptions } from '../config/configuration-options.type';
 import { CLI_ARGS } from './args';
-import { CliArgs, parseArgs } from './parse-args';
+import { convertConfig } from './convert';
+import { modify } from './modify';
 
+/* tslint:disable:no-console **/
+interface CliArgs extends Arguments, ConfigurationOptions {
+  file: string;
+}
 parseArgs(CLI_ARGS.argv as CliArgs);
+
+function parseArgs(args: CliArgs) {
+  const filePath: string = args.file;
+
+  if (args.modify) {
+    const outputString = modify(filePath, args);
+    writeFileSync(filePath, outputString);
+    console.log(`dumped to ${filePath}`);
+  } else {
+    const outputString = convertConfig(filePath, args);
+    if (args.output) {
+      writeFileSync(args.output, outputString);
+      console.log(`dumped to ${args.output}`);
+    } else {
+      console.log(outputString);
+    }
+  }
+}
