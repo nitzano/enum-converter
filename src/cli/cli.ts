40903aa3a298577900161d4c555f887c215892b7
nitzan <this.vptr@gmail.com>
2018-05-09 23:40:13 +0300

    bugfix

--- src/cli/cli.ts
+++ src/cli/cli.ts
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
 import { CLI_ARGS } from './args';
-import { EnumcArgs, parseArgs } from './parse-args';
+import { CliArgs, parseArgs } from './parse-args';
 
-parseArgs(CLI_ARGS.argv as EnumcArgs);
+parseArgs(CLI_ARGS.argv as CliArgs);
