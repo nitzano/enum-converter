af2a2521d59733dfd9cc5c5eb55f30cc99fb347d
nitzan <this.vptr@gmail.com>
2018-05-19 17:05:47 +0300

    lint

--- src/parsers/json/json.parser.spec.ts
+++ src/parsers/json/json.parser.spec.ts
@@ -1,8 +1,7 @@
 import * as path from 'path';
-
+import { EnumFile } from '../../models/enum-file/enum-file.model';
 import { EnumValue } from '../../models/enum-value/enum-value.model';
 import { JsonParser } from './json.parser';
-import { EnumFile } from '../../models/enum-file/enum-file.model';
 
 describe('Json Parser', () => {
   let parser: JsonParser;
