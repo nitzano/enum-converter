96ef55b0437ac349833c8dd05e99319e7713a641
nitzan <this.vptr@gmail.com>
2018-05-19 18:54:05 +0300

    removed language utils

--- src/utils/language-utils.ts
+++
@@ -1,9 +0,0 @@
-import { Language, LanguageSuffix } from './language.enums';
-
-export function languageToSuffix(language: Language): LanguageSuffix {
-  return LanguageSuffix[language as any] as LanguageSuffix;
-}
-
-export function suffixToLanguage(suffix: LanguageSuffix): Language {
-  return LanguageSuffix[suffix as any] as Language;
-}
