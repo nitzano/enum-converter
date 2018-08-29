8ee46a5b7fa88e961ed085277c2de3acee5dc62e
Nitzan Ohana <this.vptr@gmail.com>
2018-08-29 16:31:54 +0300

    removed unused
    Signed-off-by: Nitzan Ohana <this.vptr@gmail.com>

--- src/cli/cli.ts
+++ src/cli/cli.ts
@@ -1,13 +1,8 @@
 #!/usr/bin/env node
-import { writeFileSync } from 'fs';
 import { Arguments } from 'yargs';
-import {
-  ConfigurationOptions,
-  FileOptions
-} from '../config/configuration-options.type';
+import { FileOptions } from '../config/configuration-options.type';
 import { CLI_ARGS } from './args';
 import { convertApi } from './convert';
-import { modify } from './modify';
 
 export type YargsConfiguration = FileOptions & Arguments;
 
