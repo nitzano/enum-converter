8907aa470478ac87d47510418ebb80cd1f62faea
Nitzan Ohana <this.vptr@gmail.com>
2019-02-20 16:27:54 +0200

    remove old static

--- packages/enum-converter-api/src/app.js
+++ packages/enum-converter-api/src/app.js
@@ -1,7 +1,6 @@
 import express from 'express';
 import morgan from 'morgan';
 import apiRouter from './app/api';
-import { join } from 'path';
 
 // create the app
 export const app = express();
@@ -16,13 +15,8 @@ app.set('json spaces', 2);
 // routes
 app.use('/api', apiRouter);
 
-// react
-const reactBuildFolder = join(__dirname, '../../enum-server-web/build');
-console.log(`Serving static from ${reactBuildFolder}`);
-app.use(express.static(reactBuildFolder));
-
 // static
-app.use(express.static('static'));
+app.use(express.static());
 
 app.use((err, req, res, next) => {
   res.status(err.status ? err.status : 500).json({ error: err.message });
