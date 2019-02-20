06597c0d6175889c3f7f161c5f7bd97c8225a89d
Nitzan Ohana <this.vptr@gmail.com>
2019-02-20 15:08:29 +0200

    updates jest script

--- packages/enum-converter/jest.config.js
+++ packages/enum-converter/jest.config.js
@@ -1,16 +1,4 @@
 module.exports = {
+  preset: 'ts-jest',
   testEnvironment: 'node',
-  verbose: true,
-  transform: {
-    '^.+\\.tsx?$': 'ts-jest'
-  },
-  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$',
-  testPathIgnorePatterns: [
-    '/node_modules/',
-    '/__tests__/prod/',
-    '/__tests__/utils/',
-    '/__tests__/samples/',
-    '/build/'
-  ],
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node']
 };
