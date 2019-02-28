90489961c727c9289b529282c768bcb0019697b7
Nitzan Ohana <this.vptr@gmail.com>
2019-02-28 16:43:25 +0200

    add mockers

--- packages/enum-converter-api/src/app/schemas/schema.js
+++ packages/enum-converter-api/src/app/schemas/schema.js
@@ -1,11 +1,17 @@
-import { ApolloServer, makeExecutableSchema } from 'apollo-server-express';
+import {
+  addMockFunctionsToSchema,
+  ApolloServer,
+  makeExecutableSchema
+} from 'apollo-server-express';
+import { convertTypeDefs } from './convert';
 import { queryResolvers, queryTypeDef } from './query';
 import { versionResolvers, versionTypeDefs } from './version';
 
-const typeDefs = [queryTypeDef, versionTypeDefs];
+const typeDefs = [queryTypeDef, versionTypeDefs, convertTypeDefs];
 
 const resolvers = [queryResolvers, versionResolvers];
 
 const schema = makeExecutableSchema({ typeDefs, resolvers });
+addMockFunctionsToSchema({ schema });
 
 export const apolloServer = new ApolloServer({ schema });
