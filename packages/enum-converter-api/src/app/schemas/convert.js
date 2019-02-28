ee738b0e601beec54eae0c97006e2066c27d3771
Nitzan Ohana <this.vptr@gmail.com>
2019-02-28 17:50:48 +0200

    added string style

--- packages/enum-converter-api/src/app/schemas/convert.js
+++ packages/enum-converter-api/src/app/schemas/convert.js
@@ -1,20 +1,28 @@
 import { gql } from 'apollo-server-express';
-import { dumpers, parsers } from 'enum-converter';
-import { map, values } from 'lodash';
-// console.log('enum', parsers, dumpers);
-
-console.log('parsers', map(values(parsers), 'language'));
+import { dumpers, parsers, StringStyle } from 'enum-converter';
+import { entries, map, values } from 'lodash';
 
 export const convertTypeDefs = gql`
   extend type Query {
     parsers: [String]
     dumpers: [String]
+    stringStyles: [StringStyle]
+  }
+
+  type StringStyle {
+    name: String
+    label: String
   }
 `;
 
 export const convertResolvers = {
   Query: {
     parsers: () => map(values(parsers), 'language'),
-    dumpers: () => map(values(dumpers), 'language')
+    dumpers: () => map(values(dumpers), 'language'),
+    stringStyles: () =>
+      map(entries(StringStyle), element => ({
+        name: element[1],
+        label: element[0]
+      }))
   }
 };
