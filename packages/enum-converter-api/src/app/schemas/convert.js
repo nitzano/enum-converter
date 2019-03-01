1dc9202e7168c875ca870be156a8245688255314
Nitzan Ohana <this.vptr@gmail.com>
2019-03-01 12:45:04 +0200

    added resolvers

--- packages/enum-converter-api/src/app/schemas/convert.js
+++ packages/enum-converter-api/src/app/schemas/convert.js
@@ -1,16 +1,33 @@
 import { gql } from 'apollo-server-express';
-import { dumpers, parsers, StringStyle } from 'enum-converter';
+import {
+  dumpers,
+  EnumsOrder,
+  LanguageSuffix,
+  parsers,
+  StringStyle,
+  ValuesOrder
+} from 'enum-converter';
 import { entries, map, values } from 'lodash';
+
+function flatOption(obj) {
+  return map(entries(obj), element => ({
+    value: element[1],
+    label: element[0]
+  }));
+}
 
 export const convertTypeDefs = gql`
   extend type Query {
     parsers: [String]
     dumpers: [String]
-    stringStyles: [StringStyle]
+    stringStyles: [SelectOption]
+    enumsOrder: [SelectOption]
+    valuesOrder: [SelectOption]
+    languageSuffix: [SelectOption]
   }
 
-  type StringStyle {
-    name: String
+  type SelectOption {
+    value: String
     label: String
   }
 `;
@@ -19,10 +36,9 @@ export const convertResolvers = {
   Query: {
     parsers: () => map(values(parsers), 'language'),
     dumpers: () => map(values(dumpers), 'language'),
-    stringStyles: () =>
-      map(entries(StringStyle), element => ({
-        name: element[1],
-        label: element[0]
-      }))
+    stringStyles: () => flatOption(StringStyle),
+    enumsOrder: () => flatOption(EnumsOrder),
+    valuesOrder: () => flatOption(ValuesOrder),
+    languageSuffix: () => flatOption(LanguageSuffix)
   }
 };
