21d6294feecc5032c86647ec9402e3adf97dec2a
Nitzan Ohana <this.vptr@gmail.com>
2019-03-09 13:28:54 +0200

    removed options route

--- packages/enum-converter-api/src/app/api.js
+++ packages/enum-converter-api/src/app/api.js
@@ -1,10 +1,8 @@
 import { Router } from 'express';
 import convertRouter from './convert';
-import optionsRouter from './options';
 
 const router = Router();
 
-router.use('/options', optionsRouter);
 router.use('/convert', convertRouter);
 
 export default router;
