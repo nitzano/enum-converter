f0df47f75455de5ba43c9d68ad2c08b0a47cb7f4
Nitzan Ohana <this.vptr@gmail.com>
2019-04-20 20:24:02 +0300

    removed test

--- packages/enum-converter-web/src/index.js
+++ packages/enum-converter-web/src/index.js
@@ -4,7 +4,5 @@ import App from './app/components/App/App';
 import './index.scss';
 import registerServiceWorker from './registerServiceWorker';
 
-console.log('test');
-
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
