884186776098181a3ca9022309b655ab0419724b
Nitzan Ohana <this.vptr@gmail.com>
2019-07-27 09:58:56 +0300

    updated api version

--- packages/enum-converter-api/src/app/schemas/version.js
+++ packages/enum-converter-api/src/app/schemas/version.js
@@ -1,8 +1,8 @@
-import { gql } from 'apollo-server-express';
+import { gql } from "apollo-server-express";
 
 function getVersion() {
   // TODO: use something else than require.resolve
-  return '1.3.5';
+  return "1.3.6";
 }
 
 export const versionTypeDefs = gql`
