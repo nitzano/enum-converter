3112c2803a9e6edefa91f2051a24f7c6c1b0c19a
Nitzan Ohana <16689354+nitzano@users.noreply.github.com>
2021-01-30 14:39:08 +0200

    define app

--- packages/enum-converter-api/src/app.js
+++ packages/enum-converter-api/src/app.js
@@ -1,27 +1,29 @@
-import express from 'express';
-import morgan from 'morgan';
-import apiRouter from './app/api';
-import { apolloServer } from './app/schemas/schema';
+import express from "express";
+import morgan from "morgan";
+import apiRouter from "./app/api";
+import { apolloServer } from "./app/schemas/schema";
 
 export const app = express();
 
 // middleware
 app.use(express.json());
-app.use(morgan('tiny'));
+app.use(morgan("tiny"));
 
 // settings
-app.set('json spaces', 2);
+app.set("json spaces", 2);
 
 // routes
-app.use('/api', apiRouter);
+app.use("/api", apiRouter);
 
 // graphql
-apolloServer.applyMiddleware({ app, path: '/api/graphql' });
+apolloServer.applyMiddleware({ app, path: "/api/graphql" });
 
 // static
-app.use(express.static('static'));
+app.use(express.static("static"));
 
 app.use((err, req, res, next) => {
   res.status(err.status ? err.status : 500).json({ error: err.message });
   next(err);
 });
+
+module.exports = app;
