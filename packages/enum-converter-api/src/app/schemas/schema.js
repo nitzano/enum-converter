cabf78d9fb4280061701cef82f862a1024f2dfd4
Nitzan Ohana <16689354+nitzano@users.noreply.github.com>
2021-08-05 12:15:49 +0300

    prepare just one

--- packages/enum-converter-api/src/app/schemas/schema.js
+++ packages/enum-converter-api/src/app/schemas/schema.js
@@ -1,23 +1,21 @@
-import { ApolloServer, makeExecutableSchema } from 'apollo-server-express';
-import { convertResolvers, convertTypeDefs } from './convert';
-import { optionsResolvers, optionsTypeDefs } from './options';
-import { queryResolvers, queryTypeDef } from './query';
-import { versionResolvers, versionTypeDefs } from './version';
+import { ApolloServer } from "apollo-server-express";
+import { convertResolvers, convertTypeDefs } from "./convert";
+import { optionsResolvers, optionsTypeDefs } from "./options";
+import { queryResolvers, queryTypeDef } from "./query";
+import { versionResolvers, versionTypeDefs } from "./version";
 
 const typeDefs = [
   queryTypeDef,
   versionTypeDefs,
   convertTypeDefs,
-  optionsTypeDefs
+  optionsTypeDefs,
 ];
 
 const resolvers = [
   queryResolvers,
   versionResolvers,
   convertResolvers,
-  optionsResolvers
+  optionsResolvers,
 ];
 
-const schema = makeExecutableSchema({ typeDefs, resolvers });
-
-export const apolloServer = new ApolloServer({ schema });
+export const apolloServer = new ApolloServer({ typeDefs, resolvers });
