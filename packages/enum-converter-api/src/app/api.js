c1fe3489879f3b362ab64440272742a145bff419
Nitzan Ohana <16689354+nitzano@users.noreply.github.com>
2021-08-05 15:54:29 +0300

    add enumc banner

--- packages/enum-converter-api/src/app/api.js
+++ packages/enum-converter-api/src/app/api.js
@@ -1,5 +1,9 @@
-import { Router } from 'express';
+import { Router } from "express";
 
 const router = Router();
+
+router.get("/", (_, res) => {
+  res.send("enum conveter api");
+});
 
 export default router;
