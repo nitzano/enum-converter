5124fd94fc1276c47d326ad5a6ae2040f498d861
Nitzan Ohana <16689354+nitzano@users.noreply.github.com>
2021-08-05 16:25:30 +0300

    Revive vercel (#72)
    * start
    
    * start
    
    Signed-off-by: Nitzan Ohana <16689354+nitzano@users.noreply.github.com>
    
    * use port as callbar
    
    * add enumc banner
    
    * server fix

--- packages/enum-converter-api/src/server.js
+++ packages/enum-converter-api/src/server.js
@@ -3,6 +3,6 @@ import app from "./app";
 
 const PORT = process.env.PORT || 5000;
 
-console.log(`Enum Server started on port ${PORT}`);
-
-app.listen(PORT);
+app.listen(PORT, () => {
+  console.log(`Enum Server started on port ${PORT}`);
+});
