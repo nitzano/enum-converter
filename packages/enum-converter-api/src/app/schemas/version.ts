5aa1669e3326aea670d8f15cc6c6034b2e0d0e98
Nitzan Ohana <16689354+nitzano@users.noreply.github.com>
2021-09-02 15:04:11 +0300

    version test

--- packages/enum-converter-api/src/app/schemas/version.ts
+++ packages/enum-converter-api/src/app/schemas/version.ts
@@ -1,8 +1,8 @@
 import { gql } from "apollo-server";
+import { version } from "enum-converter";
 
-function getVersion() {
-  // TODO: use something else than require.resolve
-  return "1.3.7-alpha";
+function getVersion(): string {
+  return version();
 }
 
 export const versionTypeDefs = gql`
